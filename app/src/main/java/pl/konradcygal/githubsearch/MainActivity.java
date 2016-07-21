fdd7e1269385be50dfe9bed7517a3974d31c5ba5
kondik19 <konrad.c92@gmail.com>
2016-07-22 00:34:31 +0200

    Set colors and searching bar

--- app/src/main/java/pl/konradcygal/githubsearch/MainActivity.java
+++ app/src/main/java/pl/konradcygal/githubsearch/MainActivity.java
@@ -1,13 +1,31 @@
 package pl.konradcygal.githubsearch;
 
+import android.app.SearchManager;
+import android.databinding.DataBindingUtil;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+
+import pl.konradcygal.githubsearch.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search, menu);
+        final SearchView searchView = (SearchView) MenuItemCompat
+                .getActionView(menu.findItem(R.id.action_search));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
     }
 }
